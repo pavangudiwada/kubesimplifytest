80a4b5e06edc61c26afc4ee9ffa4f3ccfe5fe2a9
Pavan Gudiwada <25551553+pavangudiwada@users.noreply.github.com>
2022-04-21 13:56:10 +0530

    all data

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/bogeyman/Documents/Pavan/Devops/repos/kubesimplifytest/node_modules/infima/dist/css/default/default.css'),
+  require('/home/bogeyman/Documents/Pavan/Devops/repos/kubesimplifytest/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/bogeyman/Documents/Pavan/Devops/repos/kubesimplifytest/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/home/bogeyman/Documents/Pavan/Devops/repos/kubesimplifytest/src/css/custom.css'),
+];
